81c737522adc46ffeb1441db97a06ffbd74bc58b
lsankar4033 <lsankar4033@gmail.com>
2016-12-17 10:44:11 -0800

    Refactor out common logic

--- pycc/ll_parser.py
+++ pycc/ll_parser.py
@@ -1,9 +1,5 @@
-# LL(1) might be the easiest place to start (i.e. recursive descent)
-# This document outlines LL(1) parsers (and limitations of LL(1) grammars) nicely:
+# Technique taken from this document:
 # http://web.stanford.edu/class/archive/cs/cs143/cs143.1128/handouts/090%20Top-Down%20Parsing.pdf
-
-# Note that that document gives a mechanism for determining if a grammar is LL(1), which I should implement as
-# a check here
 
 from collections import namedtuple
 from copy import deepcopy
@@ -12,12 +8,12 @@ from pycc.grammar import *
 EPSILON_CHAR = ''
 END_SYMBOL = 'EOF'
 
-# Output is a map of (nonterminal_char, terminal_char) -> Rule
+# TODO - docstring
 def build_parse_table(rules, first_sets, follow_sets):
     parse_table = {}
 
     for rule in rules:
-        first_chars = get_first_chars(first_sets, rule.exp_syms, 0)
+        (first_sets, first_chars) = _get_next_terminals(rules, first_sets, rule.exp_syms, 0)
 
         sym_char = rule.sym.char
         for c in first_chars:
@@ -31,41 +27,6 @@ def build_parse_table(rules, first_sets, follow_sets):
 
     return parse_table
 
-# TODO - currently this logic exists in 3 separate places... I should figure out how to abstract the common
-# bit out
-def get_first_chars(first_sets, syms, start_ind):
-    first_chars = set()
-
-    i = start_ind
-    has_trailing_epsilon = True
-    while has_trailing_epsilon and i < len(syms):
-        next_sym = syms[i]
-        has_trailing_epsilon = False
-
-        if type(next_sym) is TSym:
-            first_chars.add(next_sym.char)
-
-        else:
-            next_terms = first_sets[next_sym.char]
-            if EPSILON_CHAR in next_terms:
-                has_trailing_epsilon = True
-                to_add = next_terms.copy()
-                to_add.remove(EPSILON_CHAR)
-
-                first_chars |= to_add
-
-            else:
-                to_add = next_terms.copy()
-
-                first_chars |= to_add
-
-        i += 1
-
-    if has_trailing_epsilon and i is len(syms):
-        first_chars.add(EPSILON_CHAR)
-
-    return first_chars
-
 def _add_to_parse_table(parse_table, nonterm, term, rule):
     parse_table_exp = [str(s) for s in rule.exp_syms]
     if (nonterm, term) in parse_table and parse_table[(nonterm, term)] is not parse_table_exp:
@@ -75,6 +36,7 @@ def _add_to_parse_table(parse_table, nonterm, term, rule):
     parse_table[(nonterm, term)] = parse_table_exp
 
 # TODO - make sure we can't enter this method for cyclic first_set dependencies
+# I.e. if a first set depends on another first set that depends on the first one. This should be detected.
 def build_first_sets(rules):
     first_sets = {}
     nonterm_syms = set([rule.sym for rule in rules])
@@ -90,37 +52,8 @@ def _add_sym_to_first_sets(sym, rules, first_sets):
     sym_first_sets = set()
 
     for rule in sym_rules:
-        i = 0
-        has_trailing_epsilon = True
-        while has_trailing_epsilon and i < len(rule.exp_syms):
-            next_sym = rule.exp_syms[i]
-            has_trailing_epsilon = False
-
-            # NOTE this assumes that there aren't terminal epsilons in the middle of expressions. I should
-            # check this during grammar validation
-            if type(next_sym) is TSym:
-                sym_first_sets.add(next_sym.char)
-
-            else:
-                if next_sym.char not in first_sets:
-                    first_sets = _add_sym_to_first_sets(next_sym, rules, first_sets)
-
-                next_terms = first_sets[next_sym.char]
-                if EPSILON_CHAR in next_terms:
-                    has_trailing_epsilon = True
-                    to_add = next_terms.copy()
-                    to_add.remove(EPSILON_CHAR)
-
-                    sym_first_sets |= to_add
-
-                else:
-                    to_add = next_terms.copy()
-
-                    sym_first_sets |= to_add
-            i += 1
-
-        if has_trailing_epsilon and i is len(rule.exp_syms) and len(sym_first_sets) is 0:
-            sym_first_sets.add(EPSILON_CHAR)
+        (first_sets, next_terminals) = _get_next_terminals(rules, first_sets, rule.exp_syms, 0)
+        sym_first_sets |= next_terminals
 
     first_sets[sym.char] = sym_first_sets
     return first_sets
@@ -133,6 +66,7 @@ def build_follow_sets(rules, first_sets):
 
     for rule in rules:
         (follow_sets, follow_dependencies) = _process_rule_follow(rule,
+                                                                  rules,
                                                                   first_sets,
                                                                   follow_sets,
                                                                   follow_dependencies)
@@ -187,7 +121,7 @@ def topo_sort(g_in):
     return ret
 
 # TODO explanatory comment
-def _process_rule_follow(rule, first_sets, follow_sets, dependencies):
+def _process_rule_follow(rule, all_rules, first_sets, follow_sets, dependencies):
     new_dependencies = dependencies.copy()
     new_follow_sets = follow_sets.copy()
 
@@ -205,31 +139,16 @@ def _process_rule_follow(rule, first_sets, follow_sets, dependencies):
             new_dependencies[sym.char].add(rule.sym.char)
 
         else:
-            i = sym_ind + 1
-            has_trailing_epsilon = True
-            while has_trailing_epsilon and i < len(rule.exp_syms):
-                has_trailing_epsilon = False
-                next_sym = rule.exp_syms[i]
+            (first_sets, next_terminals) = _get_next_terminals(all_rules,
+                                                               first_sets,
+                                                               rule.exp_syms,
+                                                               sym_ind + 1)
 
-                if type(next_sym) is TSym:
-                    new_follow_sets[sym.char].add(next_sym.char)
-
-                else:
-                    next_terms = first_sets[next_sym.char]
-                    if EPSILON_CHAR in next_terms:
-                        has_trailing_epsilon = True
-                        to_add = next_terms.copy()
-                        to_add.remove(EPSILON_CHAR)
-                        new_follow_sets[sym.char] |= to_add
-
-                    else:
-                        to_add = next_terms.copy()
-                        new_follow_sets[sym.char] |= to_add
-
-                i += 1
-
-            if has_trailing_epsilon and i is len(rule.exp_syms) and sym != rule.sym:
+            if EPSILON_CHAR in next_terminals and sym != rule.sym:
                 new_dependencies[sym.char].add(rule.sym.char)
+
+            next_terminals.discard(EPSILON_CHAR)
+            new_follow_sets[sym.char] |= next_terminals
 
     return (new_follow_sets, new_dependencies)
 
@@ -283,6 +202,50 @@ def _add_sym_to_follow_sets(sym, rules, first_sets, follow_sets):
     follow_sets[sym.char] = sym_follow_sets
     return follow_sets
 
+def _get_next_terminals(rules, first_sets, syms, start_ind):
+    """Given a sequence of symbols and previously calculated first_sets, will determine all terminals that
+    follow immediately after the specified index. The returned set will include EPSILON_CHAR if it's possible
+    for no terminal to follow the specified index based on the syms provided.
+
+    This method returns a tuple of (first_sets, next_terminals), where first_sets will include any new
+    first_sets computed and next_terminals is the aforementioned set
+
+    This method is used in first_set computation, follow_set computation, and parse_table computation.
+    """
+    next_terminals = set()
+
+    i = start_ind
+    has_trailing_epsilon = True
+    while has_trailing_epsilon and i < len(syms):
+        next_sym = syms[i]
+        has_trailing_epsilon = False
+
+        if type(next_sym) is TSym:
+            next_terminals.add(next_sym.char)
+
+        else:
+            if next_sym.char not in first_sets:
+                first_sets = _add_sym_to_first_sets(next_sym, rules, first_sets)
+
+            next_sym_first_chars = first_sets[next_sym.char]
+            if EPSILON_CHAR in next_sym_first_chars:
+                has_trailing_epsilon = True
+                to_add = next_sym_first_chars.copy()
+                to_add.remove(EPSILON_CHAR)
+
+                next_terminals |= to_add
+
+            else:
+                to_add = next_sym_first_chars.copy()
+
+                next_terminals |= to_add
+
+        i += 1
+
+    if has_trailing_epsilon and i is len(syms):
+        next_terminals.add(EPSILON_CHAR)
+
+    return (first_sets, next_terminals)
 
 class LLParser:
     # We may want to add some helpers for converting a string to rules, etc.
@@ -312,28 +275,22 @@ class LLParser:
 
             # match
             elif parse_stack[-1] == s_list[i]:
-                print("match")
                 parse_stack.pop()
-                print(parse_stack)
                 i += 1
 
             # predict attempt
             elif parse_stack[-1] != s_list[i] and parse_stack[-1] in self.nonterminals:
-                print("predict attempt")
                 X = parse_stack.pop()
                 a = s_list[i]
 
                 # predict miss
                 if (X, a) not in self.parse_table:
-                    print("predict fail {} for {}".format(X, a))
                     return False
 
-                print("predict {} for {}".format(X, a))
                 syms = self.parse_table[(X, a)].copy()
                 syms.reverse()
 
                 parse_stack = parse_stack + [sym for sym in syms if sym is not EPSILON_CHAR]
-                print(parse_stack)
 
             # mismatch
             else:
